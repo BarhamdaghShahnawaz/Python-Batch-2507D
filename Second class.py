245fccc6dd7f5a6a09801608809b95eb1b968f44
BarhamdaghShahnawaz <brahamdaghbaloch229@gmail.com>
2025-07-09 22:59:29 +0500

    class notes are sended

---
+++ Second class.py
@@ -0,0 +1,83 @@
+# lesson 1 
+# Data Types 
+# "Data type se yeh pata chalta hai ke kisi variable ke andar kaunsa type ka data rakha gaya hai.
+#  Har type ka data system mein alag tareeqe se behave karta hai."
+
+
+#  1. String 
+# String ka matlab hota hai text ya lafz. Ye hamesha double quotes (" ") ya 
+# single quotes (' ') mein likhe jate hain.
+
+# Example: 
+# name = "Ali" 
+# message = 'Hello, World!'
+# print(name) 
+
+
+#  2. Integer 
+# Integer simple poore number hote hain — jaise 1, 5, -10. Isme decimal nahi hota. 
+
+#  Example: 
+# age = 18 
+# print(age) 
+
+
+#  3. Float 
+# Float ka matlab hota hai decimal number — jaise 3.14, 99.9, 2.5 
+
+# Example:
+# price = 19.99 
+# print(price)
+
+
+# 4. Boolean 
+# Boolean sirf do hi value hoti hai: True ya False
+# Ye mostly condition check karne mein kaam aata hai.
+
+# Example:
+# is_student = True
+# print(is_student)
+
+
+#  5. None 
+# Jab kisi variable mein kuch bhi value nahi di jati, to hum usmein None likhte hain.
+# Iska matlab hota hai: "abhi khaali hai". 
+
+# Example:
+# student_name = None
+# print(student_name) 
+
+
+# lesson 2 
+# 1. Output 
+# Python mein output ka matlab hota hai screen par koi message, result ya information show karwana. 
+# Iske liye hum print() function ka istemal karte hain.
+
+# Example:
+# print("Hello, World!")  
+
+
+# 2. Input
+# Python mein input ka matlab hota hai user se koi value ya data lena. 
+# Iske liye hum input() function ka istemal karte hain.
+
+# Example:
+# name = input("Apna naam batayein: ")
+# print("Assalamualaikum", name)
+
+
+# lesson 3 
+#  Case Sensitive 
+# Python is a case-sensitive programming language 
+# Python mein chhoti aur badi alphabets (letters) ko alag maana jaata hai.
+# Matlab Name, name, aur NAME — yeh teeno alag variables hain.
+
+# Example:
+# Name = "Ali"
+# name = "Ahmed" 
+
+# print(Name) 
+# print(name) 
+
+
+
