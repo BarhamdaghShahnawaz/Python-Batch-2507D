d59bb5d8bad67aa2a9c8f93615fd8869aa9efd63
BarhamdaghShahnawaz <brahamdaghbaloch229@gmail.com>
2025-07-11 16:00:58 +0500

    done

---
+++ Activity Class.py
@@ -0,0 +1,30 @@
+# Activity  No 1
+
+# name = input("Enter Your Name:")
+# age = input("Enter Your Age:")
+# course = input("Enter Your Course:")
+# fees = 2000
+# email = input("Enter Your Email:")
+
+# agreement = input("Do you agree to the terms and conditions? (yes/no): ")
+# scholarship = None 
+
+# print("\n----Admission Slip----")
+# print("Name:",name)
+# print("Age:",age)
+# print("Course:",course)
+# print("Fees:",fees)
+# print("Emai:",email)
+# print("Agreement:",agreement)
+# print("Scholarship",scholarship)
+# print('-----------------------\n')
+
+
+# lesson No 2
+# \n 
+# \n ek escape character hai jo Python mein use hota hai text ko agle line mein le jaane ke liye.
+
+# Example 
+# print("Hello\nWorld")
+
+ 
