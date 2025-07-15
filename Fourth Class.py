f1fe047d33e7400ea0a3e0ee020d2ba5e3280d8b
BarhamdaghShahnawaz <brahamdaghbaloch229@gmail.com>
2025-07-15 22:32:15 +0500

    special characters notes

---
+++ Fourth Class.py
@@ -0,0 +1,44 @@
+# Lesson 1 
+#  Special Character 
+
+# Special characters wo characters hote hain jo normally text nahi dikhate, 
+# balkay Python ko koi khas kaam karne ka signal dete hain. 
+# Ye mostly strings mein ya escape sequences ke roop mein use hote hain.
+
+# lesson 2 
+#  Top 5 Common Special Characters in Python 
+
+#1, \n – New Line 
+# Matlab: Line break deta hai (nayi line shuru karta hai) 
+
+# Example: 
+# print("Ibrahim\nZindabad") 
+
+ 
+# 2, \t – Tab Space 
+# Matlab: Ek tab jitni khaali jaga deta hai 
+
+# Example:
+# print("Name:\tIbrahim") 
+
+
+# 3,  \\ – Backslash Print Karne Ke Liye 
+# Matlab: Agar tum \ ko normal character ki tarah print karna chaho 
+
+# Example 
+# print("This is a backslash: \\") 
+
+
+# 4, \' – Single Quote Inside String
+# Matlab: Jab tum string mein ' lagana chaho to
+
+# Example:
+# print('Ibrahim\'s Python') 
+
+
+# 5, \" – Double Quote Inside String
+# Matlab: Jab tum string mein " " likhna chaho
+
+# Example:
+# print("He said, \"Python is cool\"") 
+
