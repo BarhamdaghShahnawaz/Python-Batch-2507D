0826a70426a9f035463a0c557fb7adf08edf5bac
BarhamdaghShahnawaz <brahamdaghbaloch229@gmail.com>
2025-07-17 22:19:24 +0500

    FIFTH CLASS NOTES DONE

---
+++ Fifth Class.py
@@ -0,0 +1,75 @@
+# Lesson 1 
+# Basic Operators 
+# Operators wo symbols hote hain jo values pe calculation ya comparison karte hain. Jaise: +, -, *, / etc. 
+
+# lesson 2 
+# 1, Addition Operator (+)
+# Inka use number pe hisaab kitab (maths) karne ke liye hota hai — jaise plus, minus, multiply, divide etc. 
+
+# 1, + (Addition operator)
+# Jab tum do ya zyada numbers ko jama (add) karna chaho, to + operator use hota hai. 
+
+# Example:
+# a = 5
+# b = 3
+# print(a + b)
+
+
+#2, Subtraction Operator (-) 
+# Ye operator minus karta hai ya ek number ko doosre se ghatata hai. 
+
+# Example:
+# a = 10
+# b = 4
+# print(a - b)
+
+
+# 3, Multiplication Operator (*)
+# Ye operator do numbers ko multiply (zaarib) karta hai. 
+
+# Example:
+# a = 7
+# b = 2
+# print(a * b)
+
+
+# 4, Division Operator (/)
+# Ye operator do numbers ko divide karta hai aur point ke sath result deta hai (float mein). 
+
+# Example:
+# a = 10
+# b = 4
+# print(a / b)
+
+
+# 5, Floor Division Operator (//)
+# Ye divide karta hai lekin point ke baad ka part hata deta hai, sirf poora number deta hai. 
+
+# Example:
+# a = 10
+# b = 3
+# print(a // b)
+
+
+# 6, Modulus Operator (%) 
+# Ye operator remainder (baqi) nikalta hai jab tum divide karte ho. 
+
+# Example:
+# a = 10
+# b = 3
+# print(a % b)
+
+
+# 7, Exponentiation Operator (**)
+# Ye operator power mein uthata hai — yani ek number ko doosre number bar multiply karta hai. 
+
+# Example:
+# a = 2
+# b = 3
+# print(a ** b)
+
+
+
+
+
+
