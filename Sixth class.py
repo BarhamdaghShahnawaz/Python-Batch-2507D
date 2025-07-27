cc68c01939f3a342f469ae360365fb587943f907
BarhamdaghShahnawaz <brahamdaghbaloch229@gmail.com>
2025-07-27 18:20:11 +0500

    sixth class note are shared

---
+++ Sixth class.py
@@ -0,0 +1,12 @@
+# # Comparison Operators 
+# #  Python mein use hote hain do values ko compare karne ke liye.
+# # Ye operators True ya False return karte hain.
+
+# | Operator | Description              | Example  | Output  |
+# | -------- | ------------------------ | -------- | ------- |
+# | `==`     | Equal to                 | `5 == 5` | `True`  |
+# | `!=`     | Not equal to             | `3 != 5` | `True`  |
+# | `>`      | Greater than             | `10 > 8` | `True`  |
+# | `<`      | Less than                | `6 < 2`  | `False` |
+# | `>=`     | Greater than or equal to | `7 >= 7` | `True`  |
+# | `<=`     | Less than or equal to    | `4 <= 3` | `False` |
