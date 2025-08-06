95927fc8f8a267cedab34875c5d533a9769c6fde
BarhamdaghShahnawaz <brahamdaghbaloch229@gmail.com>
2025-08-06 15:58:03 +0500

    class 8 notes done

---
+++ class 8.py
@@ -0,0 +1,14 @@
+#  Assignment Operator 
+# Assignment operators ka use value ko variable mein store karne ke liye hota hai. 
+# Yeh Python ko batate hain ke kya value kis variable mein daalni hai — ya phir existing 
+# value ko update kaise karna hai.
+
+# Operator	Example	Same As	Urdu Explanation
+# =	x = 5	x = 5	x mein 5 daalo
+# +=	x += 3	x = x + 3	x mein 3 jor do
+# -=	x -= 2	x = x - 2	x se 2 minus karo
+# *=	x *= 4	x = x * 4	x ko 4 se multiply karo
+# /=	x /= 2	x = x / 2	x ko 2 se divide karo
+# %=	x %= 3	x = x % 3	x ko 3 se divide karo aur remainder lo
+# **=	x **= 2	x = x ** 2	x ko square karo (ya power mein 2 uthao)
+# //=	x //= 2	x = x // 2	x ko 2 se divide karo aur floor value lo
