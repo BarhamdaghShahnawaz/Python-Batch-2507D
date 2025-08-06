0fc44eb1d2962f610db9d058b02ffb91a7f0d2e1
BarhamdaghShahnawaz <brahamdaghbaloch229@gmail.com>
2025-08-06 15:56:07 +0500

    class 9 notes

---
+++ class 9.py
@@ -0,0 +1,9 @@
+# Logical Operators 
+# ogical operators aise operators hote hain jo true ya false (Boolean values) ke basis par
+#  decision lete hain. Inka use hum conditions ke sath karte hain — jaise if, while, etc.
+
+# | Operator | Name        | Use karta hai...                                         |
+# | -------- | ----------- | -------------------------------------------------------- |
+# | `and`    | Logical AND | Dono condition true honi chahiye                         |
+# | `or`     | Logical OR  | Koi ek condition true ho to chalega                      |
+# | `not`    | Logical NOT | Condition ka ulta karta hai (true → false, false → true) |
