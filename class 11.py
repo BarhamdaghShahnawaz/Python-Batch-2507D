4b3f0b667c4c0f2109be2222b4422804a3ee6662
BarhamdaghShahnawaz <brahamdaghbaloch229@gmail.com>
2025-08-13 23:39:04 +0500

    class 11 notes uploaded

---
+++ class 11.py
@@ -0,0 +1,42 @@
+# num1 = int(input("Enter your first number:"))
+# num2 = int(input("Enter your second number:"))
+
+# print("\nArithmetic Operations")
+
+# print("Addition:", num1 + num2)
+# print("Subtraction:", num1 - num2)
+# print("Multiplication:", num1 * num2)
+# print("Division:", num1 / num2)
+# print("Floor Division:", num1 // num2)
+# print("Modulus:", num1 % num2)
+# print("Exponentiation:", num1 ** num2)
+
+
+# print("\nComparison Operations")
+# print("Equal to:", num1 == num2)
+# print("Not equal to:", num1 != num2)
+# print("Greater than:", num1 > num2)
+# print("Less than:", num1 < num2)
+# print("Greater than or equal to:", num1 >= num2)
+# print("Less than or equal to:", num1 <= num2)
+
+# print("\nLogical Operations")
+# print("And operation:", num1 > 0 and num2 > 0) 
+# print("Or operation:", num1 > 0 or num2 > 50)
+# print("Not operation on num1:", not(num1 > 50))
+
+
+# print("\nMembership Operations")
+
+# fruit = ["apple", "banana", "cherry"]
+# print("Is 'apple' in fruit list?", "apple" in fruit)
+# print("Is 'grape' not in fruit list?", "apple" not in fruit)
+
+
+# print("\nIdentity Operations") 
+
+# x = [1, 2, 3]
+# y = [3,4,5]
+
+# print("Is x identical to y?", x is y)
+# print("Is x not identical to y?", x is not y)
