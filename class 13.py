7605593a9dd4af7439fd6a532a05dee38c2757a0
BarhamdaghShahnawaz <brahamdaghbaloch229@gmail.com>
2025-08-24 15:34:55 +0500

    class 13 notes uploaded

---
+++ class 13.py
@@ -0,0 +1,21 @@
+# marks = int(input("Enter the marks: (0-100) "))
+
+# if marks >=90:
+#     print("Grade A+ - Excellent")
+
+# elif marks >=80:
+#     print("Grade A - Very Good")
+
+# elif marks >=70:
+#     print("Grade B - Good")
+
+# elif marks >=60:
+#     print("Grade C - Average")
+
+# elif marks >=50:
+#  print("Grade D - Below Average")
+
+# else:
+#    print("Grade F - Fail")
+
+   
